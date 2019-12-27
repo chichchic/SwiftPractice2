37849b51c0034ecef7bece155215713a7df8cc9d
chichchic <pch6789@naver.com>
2019-12-27 15:40:31 +0900

    꼼꼼한 재은씨의 스위프트 실전편 시작

---
+++ MyMemory/MyMemory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMemory
+//
+//  Created by 박시현 on 2019/12/27.
+//  Copyright © 2019 박시현. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
